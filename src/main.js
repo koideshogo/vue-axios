a9d556cc7cbebb325e46b635fac8788fce039141
SHOGO-KOIDE <56552221+koideshogo@users.noreply.github.com>
2021-05-30 21:54:40 +0900

    axios post get interceptors

--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/vue-http-9eec2/databases/(default)/documents";
+const axiosRequest = axios.interceptors.request.use(
+  config => {
+    console.log('interceptors request', config);
+    return config
+  },
+  error => {
+    return Promise.reject(error);
+  }
+)
+const axiosResponse = axios.interceptors.response.use(
+  response => {
+    console.log('interceptors response', response);
+    return response;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+)
+console.log(axiosRequest);
+axios.interceptors.request.eject(axiosRequest);
+axios.interceptors.response.eject(axiosResponse);
 
 new Vue({
   render: h => h(App),
